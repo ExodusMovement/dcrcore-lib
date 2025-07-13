3eea74e8f10aa3fcb191cf8ab89683176d115335
Nikita Skovoroda <chalkerx@gmail.com>
2025-07-13 18:13:22 +0400

    drop sha256hmac/sha512hmac (unused)

--- test/crypto/hash.js
+++ test/crypto/hash.js
@@ -36,34 +36,6 @@ describe('Hash', function() {
 
   });
 
-  describe('#sha256hmac', function() {
-
-    it('computes this known big key correctly', function() {
-      var key = new Buffer('b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad' +
-        'b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad' +
-        'b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad' +
-        'b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad');
-      var data = new Buffer('');
-      Hash.sha256hmac(data, key).toString('hex')
-        .should.equal('fb1f87218671f1c0c4593a88498e02b6dfe8afd814c1729e89a1f1f6600faa23');
-    });
-
-    it.skip('computes this known empty test vector correctly', function() {
-      var key = new Buffer('');
-      var data = new Buffer('');
-      Hash.sha256hmac(data, key).toString('hex')
-        .should.equal('b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad');
-    });
-
-    it('computes this known non-empty test vector correctly', function() {
-      var key = new Buffer('key');
-      var data = new Buffer('The quick brown fox jumps over the lazy dog');
-      Hash.sha256hmac(data, key).toString('hex')
-        .should.equal('f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8');
-    });
-
-  });
-
   describe('#sha256sha256', function() {
 
     it('calculates the hash of this buffer correctly', function() {
@@ -127,24 +99,6 @@ describe('Hash', function() {
 
     it('fails when the input is not a buffer', function() {
       Hash.sha512.bind(Hash, str).should.throw('Invalid Argument');
-    });
-
-  });
-
-  describe('#sha512hmac', function() {
-
-    it.skip('calculates this known empty test vector correctly', function() {
-      var hex = 'b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4a' +
-        'c6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47';
-      Hash.sha512hmac(new Buffer([]), new Buffer([])).toString('hex').should.equal(hex);
-    });
-
-    it('calculates this known non-empty test vector correctly', function() {
-      var hex = 'c40bd7c15aa493b309c940e08a73ffbd28b2e4cb729eb94480d727e4df577' +
-        'b13cc403a78e6150d83595f3b17c4cc331f12ca5952691de3735a63c1d4c69a2bac';
-      var data = new Buffer('test1');
-      var key = new Buffer('test2');
-      Hash.sha512hmac(data, key).toString('hex').should.equal(hex);
     });
 
   });
